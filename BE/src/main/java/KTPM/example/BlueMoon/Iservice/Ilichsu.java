914df0fabc0f036e3aee1cddeadb056a6c5fa768
Roku2004 <tanakaroku2004@gmail.com>
2025-05-31 21:39:40 +0700

    update folder

---
+++ BE/src/main/java/KTPM/example/BlueMoon/Iservice/Ilichsu.java
@@ -0,0 +1,6 @@
+package KTPM.example.BlueMoon.Iservice;
+
+public interface Ilichsu {
+    boolean addlichsuthaydoi(int loaithaydoi, int hokhauid, String cccd, String qhevschuho);
+    boolean updateLichsuthaydoi(int id, int loaithaydoi, String cccd, String qhevschuho);
+}
